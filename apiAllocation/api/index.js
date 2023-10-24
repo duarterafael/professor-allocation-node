cdd4a98a792d89284d101aa2c32236ce7db1e203
cursoversionamento <cursoversionamento@gmail.com>
2023-10-23 21:05:56 -0300

    fixing cors bug

--- apiAllocation/api/index.js
+++ apiAllocation/api/index.js
@@ -4,6 +4,17 @@ const routes = require('./routes');
 const app = express();
 const port = 8000;
 
+const cors = require('cors');
+
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    next();
+});
+
+app.use(cors());
+
 routes(app);
 app.listen(port, () => console.log(`servidor está rodando na porta ${port}`))
 
